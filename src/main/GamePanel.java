d7e73b14bef6d184fb42eebe96be6177d8d903e3
MagicMike02 <micheleliquindoli@gmail.com>
2023-09-22 17:33:26 +0200

    Added a simply KeyHandler for the movement and updated the gameloop and update method to work accordingly to the key events

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel implements Runnable{
+
     // SCREEN SETTINGS
     final int originalTileSizes = 16; //16x16 pixels
     final int scale = 3; // scale the pixels in bigger resolution
@@ -13,12 +14,23 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; //768 pixels
     final int screenHeight = tileSize * maxScreenRow; //576 pixels
 
+    int FPS = 60;
+    private static final long ONE_SEC_IN_NANOSEC = 1000000000;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -28,26 +40,65 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
+        double drawInterval = (double)ONE_SEC_IN_NANOSEC / FPS;
+        double delta = 0;
+
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        long timer = 0;
+        int drawCount = 0;
+
         while(gameThread != null){ // Game Loop
 
-            //UPDATE: update information such as character position
-            update();
-            //DRAW: draw the screen with the uploaded information
-            repaint(); // call paintComponent method
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                //UPDATE: update information such as character position
+                this.update();
+                //DRAW: draw the screen with the uploaded information
+                repaint(); // call paintComponent method
+
+                delta--;
+                drawCount++;
+            }
+
+            if(timer >= ONE_SEC_IN_NANOSEC) {
+                System.out.println("FPS: " + drawCount);
+                timer = 0;
+                drawCount = 0;
+            }
         }
     }
 
-    public void update(){
-
+    public void update() {
+        if (keyH.upPressed) {
+            playerY -= playerSpeed;
+        }
+        else if (keyH.downPressed) {
+            playerY += playerSpeed;
+        }
+        else if (keyH.leftPressed) {
+            playerX -= playerSpeed;
+        }
+        else if (keyH.rightPressed) {
+            playerX += playerSpeed;
+        }
     }
+
+
     public void paintComponent(Graphics g){
 
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
-
         g2.setColor(Color.white);
-        g2.fillRect(100, 100 , tileSize, tileSize);
+
+        g2.fillRect(playerX, playerY , tileSize, tileSize);
         g2.dispose();
 
     }
